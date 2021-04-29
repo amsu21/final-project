4417f6cc156b724c05dbda7f158102c7ec0d2622
Amsu Warner <aswb4@hotmail.com>
2021-04-28 21:27:48 -0400

    Added Get /game test

--- src/test/java/com/company/AmsuWarnerCapstone/controller/GamesControllerTest.java
+++ src/test/java/com/company/AmsuWarnerCapstone/controller/GamesControllerTest.java
@@ -1,7 +1,32 @@
 package com.company.AmsuWarnerCapstone.controller;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+// CLASS ANNOTATIONS
+@RunWith(SpringRunner.class)
+@WebMvcTest(GamesController.class)
 
 public class GamesControllerTest {
+    // WRITING THE MOCK MVC TEST OBJECT
+    @Autowired
+    private MockMvc mockMvc;
 
+    // TEST GET "/game"
+    @Test
+    public void getGame() throws Exception {
+        // ACTION
+        mockMvc.perform(get("/game"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
 }

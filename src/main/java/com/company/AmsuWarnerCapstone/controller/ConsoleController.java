dfed041bf8e78cf32bc5ac2fbb4b55f9cc53a1de
Amsu Warner <aswb4@hotmail.com>
2021-04-28 21:08:15 -0400

    Added get console URI to the console controller.

--- src/main/java/com/company/AmsuWarnerCapstone/controller/ConsoleController.java
+++ src/main/java/com/company/AmsuWarnerCapstone/controller/ConsoleController.java
@@ -1,4 +1,26 @@
 package com.company.AmsuWarnerCapstone.controller;
 
+import com.company.AmsuWarnerCapstone.dto.Consoles;
+import com.company.AmsuWarnerCapstone.dto.Games;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class ConsoleController {
+    // private final List<Consoles> consolesList;
+    private static int idCounter = 1;
+
+    // GET ALL CONSOLES
+    // METHOD NOTATIONS
+    @RequestMapping(value = "/console", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public Consoles getConsole() {
+        // return Consoles;
+        return null;
+    }
 }

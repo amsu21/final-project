0ade0bd1ebf75c3dd5ec6aa920ddc256cf7187c6
Amsu Warner <aswb4@hotmail.com>
2021-04-28 21:36:22 -0400

    Added test for GET /tshirt

--- src/test/java/com/company/AmsuWarnerCapstone/controller/TShirtControllerTest.java
+++ src/test/java/com/company/AmsuWarnerCapstone/controller/TShirtControllerTest.java
@@ -1,7 +1,32 @@
 package com.company.AmsuWarnerCapstone.controller;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+// CLASS ANNOTATIONS
+@RunWith(SpringRunner.class)
+@WebMvcTest(TShirtController.class)
 
 public class TShirtControllerTest {
+    // WRITING THE MOCK MVC TEST OBJECT
+    @Autowired
+    private MockMvc mockMvc;
 
+    // TEST GET "/tshirt"
+    @Test
+    public void getTShirt() throws Exception {
+        // ACTION
+        mockMvc.perform(get("/tshirt"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
 }

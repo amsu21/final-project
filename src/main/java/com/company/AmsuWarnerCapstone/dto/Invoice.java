5d63de244331007b2e81a6a02811783390d23f8a
Amsu Warner <aswb4@hotmail.com>
2021-04-29 19:28:01 -0400

    Added VARS, GETTERS AND SETTERS, and EQUALS AND HASH CODE to the invoice pojo

--- src/main/java/com/company/AmsuWarnerCapstone/dto/Invoice.java
+++ src/main/java/com/company/AmsuWarnerCapstone/dto/Invoice.java
@@ -1,4 +1,140 @@
 package com.company.AmsuWarnerCapstone.dto;
 
+import java.math.BigDecimal;
+import java.util.Objects;
+
 public class Invoice {
+    // PROPS AND VARS
+    private int invoice_id;
+    private String name;
+    private String street;
+    private String city;
+    private String state;
+    private String zipcode;
+    private String item_type;
+    private String item_id;
+    private int quantity;
+    private BigDecimal subtotal;
+    private BigDecimal tax;
+    private BigDecimal processing_fee;
+    private BigDecimal total;
+
+    // GETTERS AND SETTERS
+
+    public int getInvoice_id() {
+        return invoice_id;
+    }
+
+    public void setInvoice_id(int invoice_id) {
+        this.invoice_id = invoice_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getItem_type() {
+        return item_type;
+    }
+
+    public void setItem_type(String item_type) {
+        this.item_type = item_type;
+    }
+
+    public String getItem_id() {
+        return item_id;
+    }
+
+    public void setItem_id(String item_id) {
+        this.item_id = item_id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(BigDecimal subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public BigDecimal getTax() {
+        return tax;
+    }
+
+    public void setTax(BigDecimal tax) {
+        this.tax = tax;
+    }
+
+    public BigDecimal getProcessing_fee() {
+        return processing_fee;
+    }
+
+    public void setProcessing_fee(BigDecimal processing_fee) {
+        this.processing_fee = processing_fee;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Invoice)) return false;
+        Invoice invoice = (Invoice) o;
+        return getInvoice_id() == invoice.getInvoice_id() && getQuantity() == invoice.getQuantity() && Objects.equals(getName(), invoice.getName()) && Objects.equals(getStreet(), invoice.getStreet()) && Objects.equals(getCity(), invoice.getCity()) && Objects.equals(getState(), invoice.getState()) && Objects.equals(getZipcode(), invoice.getZipcode()) && Objects.equals(getItem_type(), invoice.getItem_type()) && Objects.equals(getItem_id(), invoice.getItem_id()) && Objects.equals(getSubtotal(), invoice.getSubtotal()) && Objects.equals(getTax(), invoice.getTax()) && Objects.equals(getProcessing_fee(), invoice.getProcessing_fee()) && Objects.equals(getTotal(), invoice.getTotal());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getInvoice_id(), getName(), getStreet(), getCity(), getState(), getZipcode(), getItem_type(), getItem_id(), getQuantity(), getSubtotal(), getTax(), getProcessing_fee(), getTotal());
+    }
 }

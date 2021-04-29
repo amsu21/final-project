176c7219b8deb0c7c965dda6703154eb3825b4a8
Amsu Warner <aswb4@hotmail.com>
2021-04-29 19:52:58 -0400

    Set up Tax pojo

--- src/main/java/com/company/AmsuWarnerCapstone/dto/Tax.java
+++ src/main/java/com/company/AmsuWarnerCapstone/dto/Tax.java
@@ -1,4 +1,43 @@
 package com.company.AmsuWarnerCapstone.dto;
 
+import java.math.BigDecimal;
+import java.util.Objects;
+
 public class Tax {
+    // PROPS AND VARS
+    private String state;
+    private BigDecimal rate;
+
+    // GETTERS AND SETTERS
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public BigDecimal getRate() {
+        return rate;
+    }
+
+    public void setRate(BigDecimal rate) {
+        this.rate = rate;
+    }
+
+    // EQUALS AND HASH CODE
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Tax)) return false;
+        Tax tax = (Tax) o;
+        return Objects.equals(getState(), tax.getState()) && Objects.equals(getRate(), tax.getRate());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getState(), getRate());
+    }
 }

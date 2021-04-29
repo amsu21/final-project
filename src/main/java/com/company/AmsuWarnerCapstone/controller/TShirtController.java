8cefea998fa232a26f637992ad213d7c8b33c1c5
Amsu Warner <aswb4@hotmail.com>
2021-04-29 18:46:40 -0400

    Autowired TShirtDao in the TShirt controller.

--- src/main/java/com/company/AmsuWarnerCapstone/controller/TShirtController.java
+++ src/main/java/com/company/AmsuWarnerCapstone/controller/TShirtController.java
@@ -1,21 +1,26 @@
 package com.company.AmsuWarnerCapstone.controller;
 
+import com.company.AmsuWarnerCapstone.dao.TShirtInventoryDao;
 import com.company.AmsuWarnerCapstone.dto.TShirt;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class TShirtController {
-    private static int idCounter = 1;
+    @Autowired
+    TShirtInventoryDao tShirtInventoryDao;
 
     // GET ALL TSHIRTS
     // METHOD NOTATIONS
     @RequestMapping(value = "/tshirt", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.OK)
-    public TShirt getTShirt() {
-        return null;
+    public List<TShirt> getTShirt() {
+        return tShirtInventoryDao.getAllTShirts();
     }
 }

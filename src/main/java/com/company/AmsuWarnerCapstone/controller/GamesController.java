2ef32a09c79970d7f2e11c59d5c7947eac5cc450
Amsu Warner <aswb4@hotmail.com>
2021-04-28 21:12:53 -0400

    Added get games to the games controller

--- src/main/java/com/company/AmsuWarnerCapstone/controller/GamesController.java
+++ src/main/java/com/company/AmsuWarnerCapstone/controller/GamesController.java
@@ -1,4 +1,21 @@
 package com.company.AmsuWarnerCapstone.controller;
 
+import com.company.AmsuWarnerCapstone.dto.Games;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class GamesController {
+    private static int idCounter = 1;
+
+    // GET ALL GAMES
+    // METHOD NOTATION
+    @RequestMapping(value = "/game", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public Games getGames() {
+        return null;
+    }
 }

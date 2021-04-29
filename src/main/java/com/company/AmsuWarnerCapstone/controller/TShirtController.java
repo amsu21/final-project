f08b598b3930d541ef41166f1c2bd61415e16930
Amsu Warner <aswb4@hotmail.com>
2021-04-28 21:16:27 -0400

    Added get tshirt to tshirt controller

--- src/main/java/com/company/AmsuWarnerCapstone/controller/TShirtController.java
+++ src/main/java/com/company/AmsuWarnerCapstone/controller/TShirtController.java
@@ -1,4 +1,21 @@
 package com.company.AmsuWarnerCapstone.controller;
 
+import com.company.AmsuWarnerCapstone.dto.TShirt;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TShirtController {
+    private static int idCounter = 1;
+
+    // GET ALL TSHIRTS
+    // METHOD NOTATIONS
+    @RequestMapping(value = "/tshirt", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public TShirt getTShirt() {
+        return null;
+    }
 }

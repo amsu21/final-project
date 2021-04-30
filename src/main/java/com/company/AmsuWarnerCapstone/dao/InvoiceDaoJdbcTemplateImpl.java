bd9ab3c30170b3469cf5be6fee200101d6cf0f53
Amsu Warner <aswb4@hotmail.com>
2021-04-29 20:41:17 -0400

    Set up row mapper in Invoice JDBC Template

--- src/main/java/com/company/AmsuWarnerCapstone/dao/InvoiceDaoJdbcTemplateImpl.java
+++ src/main/java/com/company/AmsuWarnerCapstone/dao/InvoiceDaoJdbcTemplateImpl.java
@@ -1,4 +1,61 @@
 package com.company.AmsuWarnerCapstone.dao;
 
+import com.company.AmsuWarnerCapstone.dto.Invoice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Repository
 public class InvoiceDaoJdbcTemplateImpl {
+    // PREPARED STATEMENTS
+    private static final String INSERT_INVOICE_SQL =
+            "Insert into invoice (name, street, city, state, zipcode, item_type, item_id, unit_price, quantity, subtotal, tax, processing_fee, total) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    private static final String SELECT_INVOICE_SQL =
+            "Select * From invoice where invoice_id = ?";
+    private static final String SELECT_ALL_INVOICE_SQL =
+            "Select * From invoice";
+    private static final String DELETE_INVOICE_SQL =
+            "Delete from invoice where invoice_id = ?";
+    private static final String UPDATE_GAME_SQL =
+            "Update invoice set name = ?, street = ?, city = ?, state = ?, zipcode = ?, item_type = ?, item_id = ?, unit_price = ?, quantity = ?, subtotal = ?, tax = ?, processing_fee = ?, total = ?, where invoice_id = ?";
+
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public InvoiceDaoJdbcTemplateImpl (JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Invoice getInvoice(int id) {
+
+    }
+
+
+
+
+
+    //  ROW MAPPER
+    private Invoice mapToRowInvoice(ResultSet rs, int rowNumber) throws SQLException {
+        Invoice invoice = new Invoice();
+        invoice.setName(rs.getString("name"));
+        invoice.setStreet(rs.getString("street"));
+        invoice.setCity(rs.getString("city"));
+        invoice.setState(rs.getString("state"));
+        invoice.setZipcode(rs.getString("zipcode"));
+        invoice.setItem_type(rs.getString("item_type"));
+        invoice.setItem_id(rs.getInt("item_id"));
+        invoice.setUnit_price(rs.getBigDecimal("unit_price"));
+        invoice.setQuantity(rs.getInt("quantity"));
+        invoice.setSubtotal(rs.getBigDecimal("subtotal"));
+        invoice.setTax(rs.getBigDecimal("tax"));
+        invoice.setProcessing_fee(rs.getBigDecimal("processing_fee"));
+        invoice.setTotal(rs.getBigDecimal("total"));
+
+        return invoice;
+    }
 }

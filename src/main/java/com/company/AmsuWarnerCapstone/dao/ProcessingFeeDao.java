672571ccb68a282fbf0ef0c4a4bff571a1830514
Amsu Warner <aswb4@hotmail.com>
2021-04-29 21:49:25 -0400

    Set up TaxDao and ProcessingFeeDao

--- src/main/java/com/company/AmsuWarnerCapstone/dao/ProcessingFeeDao.java
+++ src/main/java/com/company/AmsuWarnerCapstone/dao/ProcessingFeeDao.java
@@ -1,4 +1,10 @@
 package com.company.AmsuWarnerCapstone.dao;
 
+import com.company.AmsuWarnerCapstone.dto.ProcessingFee;
+
+import java.math.BigDecimal;
+
 public interface ProcessingFeeDao {
+    ProcessingFee getProcessingFee(String productType);
+
 }
--- src/main/java/com/company/AmsuWarnerCapstone/dao/TaxDao.java
+++ src/main/java/com/company/AmsuWarnerCapstone/dao/TaxDao.java
@@ -1,4 +1,7 @@
 package com.company.AmsuWarnerCapstone.dao;
 
+import com.company.AmsuWarnerCapstone.dto.Tax;
+
 public interface TaxDao {
+    Tax getTax(String state);
 }

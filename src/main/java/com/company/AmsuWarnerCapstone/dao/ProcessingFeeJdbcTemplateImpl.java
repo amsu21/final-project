ad8a0c3117606ca14e212a9c6a3e12809abcb47c
Amsu Warner <aswb4@hotmail.com>
2021-04-29 22:26:57 -0400

    Set up ProcessingFee JDBC Template. Needs Prepared Statements.

--- src/main/java/com/company/AmsuWarnerCapstone/dao/ProcessingFeeJdbcTemplateImpl.java
+++ src/main/java/com/company/AmsuWarnerCapstone/dao/ProcessingFeeJdbcTemplateImpl.java
@@ -1,4 +1,39 @@
 package com.company.AmsuWarnerCapstone.dao;
 
-public class ProcessingFeeJdbcTemplateImpl {
+import com.company.AmsuWarnerCapstone.dto.ProcessingFee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Repository
+public class ProcessingFeeJdbcTemplateImpl implements ProcessingFeeDao {
+    // PREPARED STATEMENTS
+
+
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public ProcessingFeeJdbcTemplateImpl (JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public ProcessingFee getProcessingFee(String productType) {
+        return null;
+    }
+
+
+
+    // ROW MAPPER
+    private ProcessingFee mapTorowProcessingFee(ResultSet rs, int rowNumber) throws SQLException {
+        ProcessingFee processingFee = new ProcessingFee();
+        processingFee.setProduct_type(rs.getString("product_type"));
+        processingFee.setFee(rs.getBigDecimal("fee"));
+
+        return processingFee;
+    }
 }

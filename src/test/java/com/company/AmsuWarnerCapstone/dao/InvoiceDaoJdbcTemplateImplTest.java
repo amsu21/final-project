dfc56d3a972e1cae9e938a0f9a6362a4ca6f4257
Amsu Warner <aswb4@hotmail.com>
2021-04-29 21:20:30 -0400

    Added ADD and UPDATE to the Invoice JDBC Test file

--- src/test/java/com/company/AmsuWarnerCapstone/dao/InvoiceDaoJdbcTemplateImplTest.java
+++ src/test/java/com/company/AmsuWarnerCapstone/dao/InvoiceDaoJdbcTemplateImplTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -27,5 +28,50 @@ public class InvoiceDaoJdbcTemplateImplTest {
         for (Invoice i : invoiceList) {
             incoiveDao.deleteInvoice(i.getInvoice_id());
         }
+    }
+
+    // GET ALL INVOICES
+    @Test
+    public void getAllInvoices() {
+        // ARRANGE
+        Invoice invoice = new Invoice();
+        invoice.setName("Joe");
+        invoice.setStreet("123 Blvd");
+        invoice.setCity("Norfolk");
+        invoice.setState("Va");
+        invoice.setZipcode("23504");
+        invoice.setItem_type("Game");
+        invoice.setItem_id(1);
+        invoice.setUnit_price(new BigDecimal("12.99"));
+        invoice.setQuantity(2);
+        invoice.setSubtotal(new BigDecimal("13.50"));
+        invoice.setTax(new BigDecimal("0.50"));
+        invoice.setProcessing_fee(new BigDecimal("0.5"));
+        invoice.setTotal(new BigDecimal("14.00"));
+
+        // ADD ACTION
+        incoiveDao.addInvoice(invoice);
+        invoice = new Invoice();
+    }
+
+    // UPDATE
+    @Test
+    public void updateInvoice() {
+        // ARRANGE
+        Invoice invoice = new Invoice();
+        invoice.setName("Joe");
+        invoice.setStreet("123 Blvd");
+        invoice.setCity("Norfolk");
+        invoice.setState("Va");
+        invoice.setZipcode("23504");
+        invoice.setItem_type("Game");
+        invoice.setItem_id(1);
+        invoice.setUnit_price(new BigDecimal("12.99"));
+        invoice.setQuantity(2);
+        invoice.setSubtotal(new BigDecimal("13.50"));
+        invoice.setTax(new BigDecimal("0.50"));
+        invoice.setProcessing_fee(new BigDecimal("0.5"));
+        invoice.setTotal(new BigDecimal("14.00"));
+
     }
 }

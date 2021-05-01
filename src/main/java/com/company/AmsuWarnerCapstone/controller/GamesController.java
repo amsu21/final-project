ae1b5280664bc2b1d9f8d137c7bec317bb932300
Amsu Warner <aswb4@hotmail.com>
2021-05-01 14:00:22 -0400

    Added GET by STUDIO URI

--- src/main/java/com/company/AmsuWarnerCapstone/controller/GamesController.java
+++ src/main/java/com/company/AmsuWarnerCapstone/controller/GamesController.java
@@ -2,25 +2,32 @@ package com.company.AmsuWarnerCapstone.controller;
 
 import com.company.AmsuWarnerCapstone.dao.GamesInventoryDao;
 import com.company.AmsuWarnerCapstone.dto.Games;
+import com.company.AmsuWarnerCapstone.service.ServiceLayer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class GamesController {
     @Autowired
-    GamesInventoryDao gamesInventoryDao;
+    ServiceLayer serviceLayer;
 
     // GET ALL GAMES
     // METHOD NOTATION
     @RequestMapping(value = "/game", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.OK)
     public List<Games> getGames() {
-        return gamesInventoryDao.getAllGames();
+        // return serviceLayer.getAllGames();
+        return null;
+    }
+
+    // GET GAMES BY STUDIO
+    // METHOD NOTATION
+    @RequestMapping(value = "/game/studio/{studio}", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public List<Games> getGamesByStudio(@PathVariable String studio) {
+        return serviceLayer.getGamesByStudio(studio);
     }
 }

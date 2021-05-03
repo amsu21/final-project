4a9c7aac8c5a68e990e6757d66bf12f45253a252
Amsu Warner <aswb4@hotmail.com>
2021-05-03 11:33:56 -0400

    Added new method

--- src/main/java/com/company/AmsuWarnerCapstone/controller/TShirtController.java
+++ src/main/java/com/company/AmsuWarnerCapstone/controller/TShirtController.java
@@ -2,25 +2,31 @@ package com.company.AmsuWarnerCapstone.controller;
 
 import com.company.AmsuWarnerCapstone.dao.TShirtInventoryDao;
 import com.company.AmsuWarnerCapstone.dto.TShirt;
+import com.company.AmsuWarnerCapstone.service.ServiceLayer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class TShirtController {
     @Autowired
-    TShirtInventoryDao tShirtInventoryDao;
+    ServiceLayer serviceLayer;
 
     // GET ALL TSHIRTS
     // METHOD NOTATIONS
     @RequestMapping(value = "/tshirt", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.OK)
     public List<TShirt> getTShirt() {
-        return tShirtInventoryDao.getAllTShirts();
+        return serviceLayer.getAllTShirts();
+    }
+
+    // GET ALL TSHIRTS BY COLOR
+    // METHOD NOTATION
+    @RequestMapping(value = "/tshirt/color{color}", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public List<TShirt> getTShirtsByColor(@PathVariable String color) {
+        return serviceLayer.getTShirtsByColor(color);
     }
 }

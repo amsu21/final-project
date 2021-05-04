4954041834945fd847cd675195429a29823fa938
Garland Simpkins <garsimpkins@Garlands-MacBook-Pro.local>
2021-05-04 18:35:24 -0400

    Made changes within Processingfee template

--- src/main/java/com/company/AmsuWarnerCapstone/dao/ProcessingFeeJdbcTemplateImpl.java
+++ src/main/java/com/company/AmsuWarnerCapstone/dao/ProcessingFeeJdbcTemplateImpl.java
@@ -7,10 +7,25 @@ import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 @Repository
 public class ProcessingFeeJdbcTemplateImpl implements ProcessingFeeDao {
     // PREPARED STATEMENTS
+    private static final String INSERT_PROCESSINGFEE_SQL =
+            "Insert into processing_fee (title, esrb_rating, description, price, studio, quantity) values (?, ?, ?, ?, ?, ?)";
+    private static final String SELECT_PROCESSINGFEE_SQL =
+            "Select * From processing_fee where processing_fee_id = ?";
+    private static final String SELECT_ALL_PROCESSINGFEE_SQL =
+            "Select * From game";
+    private static final String DELETE_PROCESSINGFEE_SQL =
+            "Delete from game where game_id = ?";
+    private static final String UPDATE_PROCESSINGFEE_SQL =
+            "Update processing_fee set title = ?, esrb_rating = ?, description = ?, price = ?, studio = ?, quantity = ? where game_id = ?";
+    private static final String SELECT_PROCESSINGFEE_BY_PRODUCTTYPE_SQL =
+            "Select * From processing_fee where studio = ?";
+    private static final String SELECT_PROCESSINGFEE_BY_FEE_SQL =
+            "Select * From processing_fee where esrb_rating = ?";
 
 
 
@@ -19,6 +34,31 @@ public class ProcessingFeeJdbcTemplateImpl implements ProcessingFeeDao {
     @Autowired
     public ProcessingFeeJdbcTemplateImpl (JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public ProcessingFee getProcessingFee(int id) {
+        return null;
+    }
+
+    @Override
+    public List<ProcessingFee> getAllProcessingFees() {
+        return null;
+    }
+
+    @Override
+    public ProcessingFee addProcessingFee(ProcessingFee processingFee) {
+        return null;
+    }
+
+    @Override
+    public void updateProcessingFee(ProcessingFee processingFee) {
+
+    }
+
+    @Override
+    public void deleteProcessingFee(int id) {
+
     }
 
     @Override
@@ -35,5 +75,9 @@ public class ProcessingFeeJdbcTemplateImpl implements ProcessingFeeDao {
         processingFee.setFee(rs.getBigDecimal("fee"));
 
         return processingFee;
+    }
+
+
+
     }
 }

9d1f44baf6a697f2f28345c8d6101d8b1e3fc7d2
Garland Simpkins <garsimpkins@Garlands-MacBook-Pro.local>
2021-05-04 18:38:06 -0400

    Made changes in the processingFee Controller

--- src/main/java/com/company/AmsuWarnerCapstone/controller/ProcessingFeeController.java
+++ src/main/java/com/company/AmsuWarnerCapstone/controller/ProcessingFeeController.java
@@ -1,4 +1,32 @@
 package com.company.AmsuWarnerCapstone.controller;
 
+
+import com.company.AmsuWarnerCapstone.dao.ProcessingFeeDao;
+import com.company.AmsuWarnerCapstone.dto.Games;
+import com.company.AmsuWarnerCapstone.dto.ProcessingFee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class ProcessingFeeController {
+
+    @Autowired
+    ServiceLayer serviceLayer;
+
+    //GET ALL FEES
+    //METHOD NOTATION
+    @RequestMapping(value = "/fee" , method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public List<ProcessingFee> getProcessingFee() {
+        return processingFeeDao.getAllProcessingFees();
+    }
 }
+
+
+

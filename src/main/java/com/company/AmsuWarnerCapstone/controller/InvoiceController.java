a38cf5695a0e83fb0add0f0358a6f84e5515ac56
Amsu Warner <aswb4@hotmail.com>
2021-05-04 18:19:50 -0400

    Added invoice URI

--- src/main/java/com/company/AmsuWarnerCapstone/controller/InvoiceController.java
+++ src/main/java/com/company/AmsuWarnerCapstone/controller/InvoiceController.java
@@ -1,4 +1,26 @@
 package com.company.AmsuWarnerCapstone.controller;
 
+import com.company.AmsuWarnerCapstone.dto.Invoice;
+import com.company.AmsuWarnerCapstone.service.ServiceLayer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class InvoiceController {
+    @Autowired
+    ServiceLayer serviceLayer;
+
+    // GET INVOICE
+    // METHOD NOTATIONS
+    @RequestMapping(value = "/invoice", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public List<Invoice> getInvoice() {
+        return serviceLayer.getAllInvoices();
+    }
 }

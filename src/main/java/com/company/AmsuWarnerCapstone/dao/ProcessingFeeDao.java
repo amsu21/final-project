a30ce787ceb843b8c6f8b75664e8e942d181fa5e
Amsu Warner <aswb4@hotmail.com>
2021-05-04 21:00:04 -0400

    Added two methods

--- src/main/java/com/company/AmsuWarnerCapstone/dao/ProcessingFeeDao.java
+++ src/main/java/com/company/AmsuWarnerCapstone/dao/ProcessingFeeDao.java
@@ -8,17 +8,7 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public interface ProcessingFeeDao {
-    ProcessingFee getProcessingFee(int id);
-
-    List<ProcessingFee> getAllProcessingFees();
-
-    BigDecimal getFeeByType(String productType);
-
-    ProcessingFee addProcessingFee(ProcessingFee processingFee);
-
-    void updateProcessingFee(ProcessingFee processingFee);
-
-    void deleteProcessingFee(int id);
-
     ProcessingFee getProcessingFee(String productType);
+
+    BigDecimal getFeeNum(String productType);
 }

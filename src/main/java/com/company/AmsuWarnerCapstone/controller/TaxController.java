6c734a4a1781cb93a17c7b4b4ea8fa9cf955ee3b
Amsu Warner <aswb4@hotmail.com>
2021-05-04 20:59:36 -0400

    Added two methods

--- src/main/java/com/company/AmsuWarnerCapstone/controller/TaxController.java
+++
@@ -1,4 +0,0 @@
-package com.company.AmsuWarnerCapstone.controller;
-
-public class TaxController {
-}
--- src/main/java/com/company/AmsuWarnerCapstone/dao/TaxDao.java
+++ src/main/java/com/company/AmsuWarnerCapstone/dao/TaxDao.java
@@ -2,6 +2,10 @@ package com.company.AmsuWarnerCapstone.dao;
 
 import com.company.AmsuWarnerCapstone.dto.Tax;
 
+import java.math.BigDecimal;
+
 public interface TaxDao {
     Tax getTax(String state);
+
+    BigDecimal getRate(String state);
 }
